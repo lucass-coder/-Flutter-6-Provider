c3c60c2ba277ff555890283ba934bfafbe287bce
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-29 19:35:33 -0300

    Adicionando Provider.of

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,35 +1,27 @@
-import 'package:bytebank/models/saldo.dart';
 import 'package:bytebank/screens/dashboard/saldo.dart';
 import 'package:bytebank/screens/deposito/formulario.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class Dashboard extends StatelessWidget{
-  Widget build(BuildContext context){
+class Dashboard extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bytebank'),
-      ),
-      body: ListView(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topCenter,
-                  child:SaldoCard(),
-                ),
-               Consumer<Saldo>(
-                   builder: (context, saldo, child){
-                    return  ElevatedButton(
-                      child: Text('Receber depósito'),
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                          return FormularioDeposito();
-                        }));
-                      //saldo.adiciona(10);
-                  },
-                  );
-                },
-               )
-              ]
-    ));
+        appBar: AppBar(
+          title: const Text('Bytebank'),
+        ),
+        body: ListView(children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: SaldoCard(),
+          ),
+          ElevatedButton(
+            child: Text('Receber depósito'),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return FormularioDeposito();
+              }));
+              //saldo.adiciona(10);
+            },
+          ),
+        ]));
   }
 }
--- lib/screens/deposito/formulario.dart
+++ lib/screens/deposito/formulario.dart
@@ -1,5 +1,7 @@
 import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/saldo.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 const _tituloAppBar = 'Receber depósito';
 const _dicaCampoValor = '0.00';
@@ -36,7 +38,26 @@ class FormularioDeposito extends StatelessWidget {
   }
 
   _criaDeposito(context) {
-    Navigator.pop(context);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    final depositoValido = _validaDeposito(valor);
+
+    if(depositoValido){
+      _atualizaEstado(context, valor);
+      Navigator.pop(context);
+    }
+
 
   }
+
+  _validaDeposito(valor){
+    final _campoPreenchido = valor != null;
+
+    return _campoPreenchido;
+
+  }
+
+  _atualizaEstado(context, valor){
+    Provider.of<Saldo>(context, listen:false).adiciona(valor);
+  }
+
 }
